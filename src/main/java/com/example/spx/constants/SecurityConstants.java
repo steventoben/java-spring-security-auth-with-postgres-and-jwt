54c34559f0a1f9e4be2889e4814543e7c1753c70
Steven Toben <steventoben@gmail.com>
2022-06-07 19:11:30 -0500

    passes basic controller tests

--- src/main/java/com/example/spx/constants/SecurityConstants.java
+++ src/main/java/com/example/spx/constants/SecurityConstants.java
@@ -5,7 +5,6 @@ public final class SecurityConstants {
     //JWTs expire 36000 seconds (10 hours) after being issued
     public static final long JWT_EXPIRES_IN_SECONDS = 36000L;
     //Default strength of hashing algorithm.
-    //DO NOT CHANGE DURING PRODUCTION WITHOUT USING A DELEGATE
     public static final int BCRYPT_HASHING_STRENGTH = 10;
     //private constructor so this class can not be instantiated
     private SecurityConstants() {
